820eff32fbf11b7c464a38044dd842b463ed87d6
shithins <shithinckss2001@gmail.com>
2021-07-05 15:12:27 +0530

    modifications

--- src/components/homepage/home.js
+++ src/components/homepage/home.js
@@ -4,8 +4,10 @@ import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import MenuTwoToneIcon from '@material-ui/icons/MenuTwoTone';
 import samimg1 from '../../image/booksam1.jpeg'
 import samimg2 from '../../image/booksam2.jpeg'
+import {Redirect,useHistory } from 'react-router-dom'
 
 const Homepage = () => {
+        const history= useHistory();
     return(
         <div className="page-main">
         <div className="home-main">
@@ -38,6 +40,24 @@ const Homepage = () => {
                      used to demonstrate the visual form of a document or a typeface
                     </h5>
                     <h6>20/40/8080</h6>
+                    <div className="home-readmore-btn">
+                    <h2 onClick={()=> history.push('/posts')}>Read More..</h2>
+                </div>
+                </div>
+
+                <div className="card-main" >
+                    <div className="card-img">
+                        <img src={samimg2}/>
+                    </div>
+                    <h2>HEADING</h2>
+                    <h5>vaisakh is a very much bloody fool..
+                    , Lorem ipsum is a placeholder text commonly
+                     used to demonstrate the visual form of a document or a typeface
+                    </h5>
+                    <h6>20/40/8080</h6>
+                    <div className="home-readmore-btn">
+                    <h2 onClick={()=> history.push('/posts')}>Read More..</h2>
+                </div>
                 </div>
 
                 <div className="card-main">
@@ -50,6 +70,9 @@ const Homepage = () => {
                      used to demonstrate the visual form of a document or a typeface
                     </h5>
                     <h6>20/40/8080</h6>
+                    <div className="home-readmore-btn">
+                    <h2 onClick={()=> history.push('/posts')}>Read More..</h2>
+                </div>
                 </div>
 
                 <div className="card-main">
@@ -62,18 +85,9 @@ const Homepage = () => {
                      used to demonstrate the visual form of a document or a typeface
                     </h5>
                     <h6>20/40/8080</h6>
+                    <div className="home-readmore-btn">
+                    <h2 onClick={()=> history.push('/posts')}>Read More..</h2>
                 </div>
-
-                <div className="card-main">
-                    <div className="card-img">
-                        <img src={samimg2}/>
-                    </div>
-                    <h2>HEADING</h2>
-                    <h5>vaisakh is a very much bloody fool..
-                    , Lorem ipsum is a placeholder text commonly
-                     used to demonstrate the visual form of a document or a typeface
-                    </h5>
-                    <h6>20/40/8080</h6>
                 </div>
             
 
